2d2d086b1d96cdfc2ce0ea3cc088bf9fb4102256
TonkersTinkers <tonkerstinkers@outlook.com>
2025-10-16 16:48:39 -0700

    Clean up comments in Ux_TonkersTableTopiaRow
    Removed unnecessary comments from Ux_TonkersTableTopiaRow class.

--- TTT/Ux_TonkersTableTopiaRow.cs
+++ TTT/Ux_TonkersTableTopiaRow.cs
@@ -1,9 +1,5 @@
 using UnityEngine;
 
-/// <summary>
-/// Component attached to each row GameObject in a Ux_TableLayout.
-/// Holds the row index and can be extended for row-specific behavior or data.
-/// </summary>
 [RequireComponent(typeof(RectTransform))]
 public class Ux_TonkersTableTopiaRow : MonoBehaviour
 {
