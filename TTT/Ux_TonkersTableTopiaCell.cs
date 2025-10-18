942ff8a28fc14807474f407ada51510dc2874208
TonkersTinkers <tonkerstinkers@outlook.com>
2025-10-18 14:42:42 -0700

    Context Menu Commit

--- TTT/Ux_TonkersTableTopiaCell.cs
+++ TTT/Ux_TonkersTableTopiaCell.cs
@@ -3,13 +3,12 @@ using UnityEngine;
 [RequireComponent(typeof(RectTransform))]
 public class Ux_TonkersTableTopiaCell : MonoBehaviour
 {
-    [HideInInspector] public int rowNumberWhereThePartyIs = -1;
+    public Color backgroundColorLikeASunset = Color.white;
+    public Sprite backgroundPictureBecausePlainIsLame = null;
     [HideInInspector] public int columnNumberPrimeRib = -1;
-    [HideInInspector] public int howManyRowsAreHoggingThisSeat = 1;
     [HideInInspector] public int howManyColumnsAreSneakingIn = 1;
+    [HideInInspector] public int howManyRowsAreHoggingThisSeat = 1;
     [HideInInspector] public bool isMashedLikePotatoes = false;
     [HideInInspector] public Ux_TonkersTableTopiaCell mashedIntoWho = null;
-
-    public Sprite backgroundPictureBecausePlainIsLame = null;
-    public Color backgroundColorLikeASunset = Color.white;
+    [HideInInspector] public int rowNumberWhereThePartyIs = -1;
 }
